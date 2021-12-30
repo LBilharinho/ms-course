f9835bdf258b4649c1bbf3d272782db75de3269f
Lucas Bilharinho <bilharinho2006@hotmail.com>
2021-12-30 17:51:50 -0300

    hr worker implementation

---
+++ hr-worker/src/main/java/com/recursosHumanos/hrWorker/repositories/WorkerRepository.java
@@ -0,0 +1,8 @@
+package com.recursosHumanos.hrWorker.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.recursosHumanos.hrWorker.entities.Worker;
+
+public interface WorkerRepository extends JpaRepository<Worker, Long> {
+
+}
