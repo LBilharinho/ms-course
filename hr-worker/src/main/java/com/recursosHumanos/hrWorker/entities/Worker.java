2987e7922acdef8257bd7f561b29390b6d1568ed
Lucas Bilharinho <49289812+LBilharinho@users.noreply.github.com>
2021-12-30 22:16:06 -0300

    correcoes

--- hr-worker/src/main/java/com/recursosHumanos/hrWorker/entities/Worker.java
+++ hr-worker/src/main/java/com/recursosHumanos/hrWorker/entities/Worker.java
@@ -14,6 +14,7 @@ import javax.persistence.Table;
 public class Worker implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
